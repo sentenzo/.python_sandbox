fb3195d2332aa12694c5f85050fc18d9d56cc71f
sentenzo <kassiyskley@rambler.ru>
2022-12-10 10:09:15 +0300

    gen-ff: try @contextmanager + refactoring

--- asyncio/D1twn9kLmYg.py
+++ asyncio/D1twn9kLmYg.py
@@ -36,74 +36,102 @@ def generator():
     return "The End"
 
 
-g1 = generator()
+def test1():
+    g1 = generator()
 
-assert next(g1) == "01: simple yield"
-assert next(g1) == "02: yield + send"
-assert next(g1) == "03: x = None"
-assert next(g1) == "04: break me if you can"
-assert next(g1) == "04: break me if you can"
-assert next(g1) == "04: break me if you can"
-assert g1.send(12) == "04: break me if you can"
-assert g1.send(None) == "04: break me if you can"
-assert g1.send([1, 2, 3]) == "04: break me if you can"
-assert g1.send("break") == "05: this is the end"
-try:
-    next(g1)
-except StopIteration as e:
-    print("# StopIteration:", e.value)
+    assert next(g1) == "01: simple yield"
+    assert next(g1) == "02: yield + send"
+    assert next(g1) == "03: x = None"
+    assert next(g1) == "04: break me if you can"
+    assert next(g1) == "04: break me if you can"
+    assert next(g1) == "04: break me if you can"
+    assert g1.send(12) == "04: break me if you can"
+    assert g1.send(None) == "04: break me if you can"
+    assert g1.send([1, 2, 3]) == "04: break me if you can"
+    assert g1.send("break") == "05: this is the end"
+    try:
+        next(g1)
+    except StopIteration as e:
+        print("# StopIteration:", e.value)
 
-print("\n###########################\n")
-
-###############################################################################
-
-g2 = generator()
-assert g2.send(None) == "01: simple yield"
-assert g2.send(222) == "02: yield + send"
-assert g2.send(333) == "03: x = 333"
-assert g2.send(444) == "04: break me if you can"
-assert g2.send(555) == "04: break me if you can"
-g2.close()
-
-print("\n###########################\n")
-
-###############################################################################
-
-g3 = generator()
-assert g3.send(None) == "01: simple yield"
-assert g3.send(222) == "02: yield + send"
-assert g3.send(333) == "03: x = 333"
-assert g3.send(444) == "04: break me if you can"
-assert g3.send(555) == "04: break me if you can"
-assert (
-    g3.throw(ValueError("Hello, I'm a friendly value error 🙂")) == "05: this is the end"
-)
-try:
-    g3.send(666)
-except StopIteration as e:
-    print("# StopIteration:", e.value)
+    print("\n###########################\n")
 
 
-###############################################################################
+def test2():
+    g2 = generator()
+    assert g2.send(None) == "01: simple yield"
+    assert g2.send(222) == "02: yield + send"
+    assert g2.send(333) == "03: x = 333"
+    assert g2.send(444) == "04: break me if you can"
+    assert g2.send(555) == "04: break me if you can"
+    g2.close()
 
-def say_hallo_gen():
-    first_name = yield "say your first name"
-    last_name = yield "say your last name"
-    return f"Hello, {first_name} {last_name}"
-
-
-try:
-    gen = say_hallo_gen()
-    request = gen.send(None)
-    while True:
-        print(request)
-        request = gen.send(input())
-except StopIteration as e:
-    print(e.value)
+    print("\n###########################\n")
 
 
-###############################################################################
+def test3():
+    g3 = generator()
+    assert g3.send(None) == "01: simple yield"
+    assert g3.send(222) == "02: yield + send"
+    assert g3.send(333) == "03: x = 333"
+    assert g3.send(444) == "04: break me if you can"
+    assert g3.send(555) == "04: break me if you can"
+    assert (
+        g3.throw(ValueError("Hello, I'm a friendly value error 🙂"))
+        == "05: this is the end"
+    )
+    try:
+        g3.send(666)
+    except StopIteration as e:
+        print("# StopIteration:", e.value)
 
-import time
 
-def big_task(stage_duration=)
+def all_tests():
+    test1()
+    test2()
+    test3()
+
+
+def main_say_hallo():
+    def say_hallo_gen():
+        first_name = yield "say your first name"
+        last_name = yield "say your last name"
+        return f"Hello, {first_name} {last_name}"
+
+    try:
+        gen = say_hallo_gen()
+        request = gen.send(None)
+        while True:
+            print(request)
+            request = gen.send(input())
+    except StopIteration as e:
+        print(e.value)
+
+
+def main_try_context():
+    import datetime
+    from contextlib import contextmanager
+
+    @contextmanager
+    def timer(name: str):
+        dt_start = datetime.datetime.now()
+        try:
+            yield
+        finally:
+            dt_stop = datetime.datetime.now()
+            print(f"Timer {name} was started at: {dt_start}")
+            print(f"Timer {name} is stopped at: {dt_stop}")
+            print(f"Total time passed: {dt_stop - dt_start}")
+
+    with timer("test"):
+        acc = 0
+        for i in range(10_000):
+            acc += i
+            acc %= 119
+        print(f"acc={acc}")
+
+
+if __name__ == "__main__":
+    # all_tests()
+    # main_say_hallo()
+    main_try_context()
