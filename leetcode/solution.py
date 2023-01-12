c58e0b1f791c968f4e62ee48998a38f0534a9521
sentenzo <kassiyskley@rambler.ru>
2023-01-12 08:17:15 +0300

    LeetCode binary tree draft template

--- leetcode/solution.py
+++ leetcode/solution.py
@@ -3,35 +3,40 @@ from typing import *
 from functools import *
 from heapq import *
 
-from singly_linked_list import *
+from lc.singly_linked_list import *
+from lc.binary_tree import *
 
-from math import factorial
 
 class Solution:
     """
-    https://leetcode.com/problems/remove-nodes-from-linked-list/
+    https://leetcode.com/problems/increasing-order-search-tree/
     """
-    def removeNodes(self, head: Optional[ListNode], mx = float("-inf")) -> Optional[ListNode]:
-        
-        lst = []
-        cur = head
-        while cur:
-            v = cur.val
-            while lst and lst[-1] < v:
-                lst.pop()
-            lst.append(v)
-            cur = cur.next
-        
-        root = ListNode(None)
-        cur = root
-        for x in lst:
-            cur.next = ListNode(x)
-            cur = cur.next
-        return root.next
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        que = [root]
+        ans = pos = None
+        while que:
+            cur = que.pop()
+            if isinstance(cur, tuple):
+                if ans:
+                    pos.right = cur[0]
+                    pos = pos.right
+                    pos.left = None
+                else:
+                    ans = pos = cur[0]
+                    pos.left = None
+            else:
+                if cur.right:
+                    que.append(cur.right)
+                que.append((cur,))
+                if cur.left:
+                    que.append(cur.left)
+        return ans
 
+inp = stringToTreeNode("[5,3,6,2,4,null,8,1,null,null,null,7,9]")
+prettyPrintTree(inp)
 
-ans = Solution().removeNodes(
-    stringToListNode("[5,2,13,3,8]")
+ans = Solution().increasingBST(
+    inp
 )
 
-prettyPrintLinkedList(ans)
+prettyPrintTree(ans)
