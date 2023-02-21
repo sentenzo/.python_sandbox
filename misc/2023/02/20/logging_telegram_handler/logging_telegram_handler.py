3031cadad47ce0ae42854c3d9a55b40125ea469f
sentenzo <kassiyskley@rambler.ru>
2023-02-21 10:51:13 +0300

    `TelegramHandler` improved

--- misc/2023/02/20/logging_telegram_handler/logging_telegram_handler.py
+++ misc/2023/02/20/logging_telegram_handler/logging_telegram_handler.py
@@ -1,9 +1,15 @@
 import logging
+import sys
 import time
 
 import telegram
+from telegram.error import TelegramError
 
 ANTISPAM_DELAY_SEC = 5 * 60
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 class TelegramHandler(logging.Handler):
@@ -16,8 +22,12 @@ class TelegramHandler(logging.Handler):
         try:
             # an independent instance of a telegram bot object
             self.bot = telegram.Bot(token)
-        except Exception:
-            pass
+        except TelegramError as ex:
+            logger.error(
+                "Failed to create a Bot instance. "
+                "Logging to Telegram won't work.\n"
+                f"TelegramError: {ex}"
+            )
 
         self.prev_msg = None
         self.prev_msg_time = 0
@@ -31,10 +41,17 @@ class TelegramHandler(logging.Handler):
             and self.prev_msg_time - time.time() < ANTISPAM_DELAY_SEC
         ):
             # we don't need the same message to be sent too often
+            logger.debug(
+                f"The same message in less, than {ANTISPAM_DELAY_SEC} secs. "
+                "Sending to Telegram chat was suppressed."
+            )
             return
         try:
             self.bot.send_message(chat_id=self.chat_id, text=msg)
             self.prev_msg = msg
             self.prev_msg_time = time.time()
-        except Exception:
-            pass
+        except TelegramError as ex:
+            logger.error(
+                "An error occured while sanding log-message "
+                f"to telegram chat: {ex}"
+            )
