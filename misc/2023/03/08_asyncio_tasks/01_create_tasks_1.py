2732a7fdfe4e16f1a8bff2fd231975f77bdef545
sentenzo <kassiyskley@rambler.ru>
2023-03-08 19:23:39 +0300

    reworked with callbacks

--- misc/2023/03/08_asyncio_tasks/01_create_tasks_1.py
+++ misc/2023/03/08_asyncio_tasks/01_create_tasks_1.py
@@ -23,32 +23,56 @@ async def runner(name):
 
 async def main():
     todo = set()
-    done = []
+    succeeded = []
+    failed = []
+    cancelled = []
+
+    def on_done_callback(task: asyncio.Task):
+        todo.discard(task)
+        if task.cancelled():
+            cancelled.append(task)
+        elif task.exception():
+            failed.append(task)
+        else:
+            succeeded.append(task)
+
     for i in range(10):
         name = f"runner {i}"
         task = asyncio.create_task(runner(name), name=name)
-
         todo.add(task)
-        task.add_done_callback(todo.discard) # todo.discard(task)
-        task.add_done_callback(done.append) #  done.append(task)
+        task.add_done_callback(on_done_callback)
 
     while todo:
         if random.random() < 0.1:
             any_task = next(iter(todo)) 
-            any_task.cancel("Because I can!")
-        await asyncio.sleep(0.05)
+            was_cancel = any_task.cancel("Because I can!")
+            if was_cancel:
+                print(
+                    "[MAIN]  :", 
+                    any_task.get_name(), 
+                    "was successfully cancelled",
+                )
+        # if we used `await task`, it could forward the Exception
+        #  but if we run tasks implicitly in background (by sleeping in main 
+        #  line), the Exception will not be forwarded, and will be stored 
+        #  in `task.exception()`
+        await asyncio.sleep(0.01)
     
     print()
     
-    for task in done:
-        if task.cancelled():
-            print(task.get_name(), "- cancelled")
-        else:
-            result = None
-            try:
-                result = task.result()
-                print(task.get_name(), "- ok, result=", result)
-            except Exception as e:
-                print(task.get_name(), "- failed with", e)
+    if succeeded:
+        print("Succeeded:")
+        for task in succeeded:
+            # if we had an Exception in task.exception(), 
+            #  then task.result() would rais it
+            print("  ", task.get_name(), "=>", task.result())
+    if cancelled:
+        print("Cancelled:")
+        for task in cancelled:
+            print("  ", task.get_name())
+    if failed:
+        print("Failed:")
+        for task in failed:
+            print("  ", task.get_name(), "=>", task.exception())
 
 asyncio.run(main())
